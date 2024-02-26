70ccccb2ecf381807dacdcdb1060ec465d94e3da
Talha Ghouri <talhaghouridev@gmail.com>
2024-02-26 05:07:52 -0800

    add pages routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,18 +1,18 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Outlet,
-} from "react-router-dom";
-import Home from "./Pages/Home/Home";
-import Layout from "@layout/layout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy } from "react";
+const Home = lazy(() => import("@pages/home"));
+const Signup = lazy(() => import("@pages/(authenication)/signup"));
+const Login = lazy(() => import("@pages/(authenication)/login"));
+const Layout = lazy(() => import("@layout/layout"));
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="" element={<Home />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Route>
       </Routes>
     </Router>
